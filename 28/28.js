11fe518bc7ed3022aadd651d64e0d6c295598bc3
yangzongjun <yangzongjun@baidu.com>
2017-07-22 00:01:22 +0800

    LeetCode每日一题：2017-07-21

--- 28/28.js
+++ 28/28.js
@@ -1,12 +1,23 @@
+// 用js函数，太简单了
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+
+// 自己写暴力判断，结果tle😂
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
 var strStr = function(haystack, needle) {
     var pos = -1;
     if (needle === '') {
         return 0;
     }
-    for (var i = haystack.length - 1; i >= 0; i--) {
+    for (var i = 0, lenI = haystack.length; i < lenI; i++) {
         if (haystack[i] === needle[0]) {
             var allEqual = true;
-            for (var j = 0, lenJ = needle.length; j < lenJ; j++) {
+            for (var j = 1, lenJ = needle.length; j < lenJ; j++) {
                 if (haystack[i + j] !== needle[j]) {
                     allEqual = false;
                     break;
@@ -20,3 +31,5 @@ var strStr = function(haystack, needle) {
     }
     return pos;
 };
+
+console.log(strStr('aaa', 'a'));
--- 28/readme.md
+++ 28/readme.md
@@ -1,9 +1,11 @@
 # description
 
+给定一个父字符串和子字符串，判断子串在父串中出现的下标
 
 # solution
 
-
+1. 用js的indexOf方法一行解决，太简单了。
+2. 自己挨个字符判断tle
 
 # conclusion
 
