e5190c735a7a78b9f58f2b64ae37232dc6829326
yangzongjun <yangzongjun@baidu.com>
2017-07-24 23:20:29 +0800

    LeetCode每日一题,js版本：2017-07-24 今天没时间做了，明天补上

--- github.sh
+++ github.sh
@@ -1,4 +1,4 @@
 # commit shell，modify commit date before exec
 git add .
-git commit -m 'LeetCode每日一题,js版本：2017-07-23 顺便补上昨天没做的题。。'
+git commit -m 'LeetCode每日一题,js版本：2017-07-24 今天没时间做了，明天补上'
 git push
