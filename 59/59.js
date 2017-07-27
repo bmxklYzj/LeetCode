ea6037b60f8c564028d4186d90e32938f0474d7c
yangzongjun <yangzongjun@baidu.com>
2017-07-27 21:01:42 +0800

    LeetCode每日一题,js版本：2017-07-26

--- 59/59.js
+++ 59/59.js
@@ -3,8 +3,44 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
+    if (n === 1) return [[1]];
+    if (n === 0) return [];
     var arr = [];
-    for (var i = 0; i < n * n; i++) {
-        var start = i, end = n - 1
+    for (var i = 0; i < n; i++) {
+        arr[i] = [];
+        for (var j = 0; j < n; j++) {
+            arr[i][j] = -1;
+        }
+    }
+    var loopCnt = 1;
+    var num = 1;
+    while (true) {
+        var i = loopCnt - 1;
+        var j = loopCnt - 1;
+        for (; i <= (n - loopCnt); i++) {
+            arr[j][i] = num;
+            num++;
+        }
+        for (i--, j++; j <= (n - loopCnt); j++) {
+            arr[j][i] = num;
+            num++;
+        }
+        for (i--, j--; i >= (loopCnt - 1); i--) {
+            arr[j][i] = num;
+            num++;
+        }
+        for (i++, j--; j >= (loopCnt); j--) {
+            arr[j][i] = num;
+            num++;
+        }
+        i++;
+        j++;
+        // console.log(i, j);
+        loopCnt++;
+        if (arr[i][j] !== -1) {
+            return arr;
+        }
     }
 };
+
+console.log(generateMatrix(1));
