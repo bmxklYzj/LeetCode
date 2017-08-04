0c2afa592311abd644bdbc87fdd4980410a34581
yangzongjun <yangzongjun@baidu.com>
2017-08-04 23:55:12 +0800

    LeetCode每日一题,js版本：2017-08-04

--- 118/118.js
+++ 118/118.js
@@ -3,22 +3,24 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
+    if (numRows === 0) {
+        return [];
+    }
     let half = ~~((numRows + 1) / 2);
     let res = [[1]];
-    for (let i = 2; i <= numRows; i++) {
+    for (let i = 1; i < numRows; i++) {
         let temp = [];
-        temp.push(1);
-        res[i-1] = temp;
-        for (let j = 1; j < half; j++) {
-            let leftTop = res[i-1][j-1] ? res[i-1][j-1] : 0;
-            let top = res[i-1][j] ? res[i-1][j] : 0;
-            let t =  leftTop + top;
-            console.log(t);
-            temp.push(t);
+        for (let j = 0; j <= i; j++) {
+            let leftTop = res[i - 1][j - 1];
+            let top = res[i - 1][j];
+            leftTop = leftTop ? leftTop : 0;
+            top = top ? top : 0;
+            temp.push(leftTop + top);
         }
-        res[i-1] = temp;
+        res.push(temp);
+        // console.log(temp);
     }
     return res;
 };
 
-console.log(generate(3));
+console.log(generate(5));
