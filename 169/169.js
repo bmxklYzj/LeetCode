6bb058304958b1b91d9a74bea5524a6af5bd3248
yangzongjun <yangzongjun@baidu.com>
2017-09-04 21:57:39 +0800

    LeetCode每日一题,js版本：2017-09-04 罪孽呀，接了新项目，比较难，在适应期，然后每天很烦，竟然已经有半个月没有做题了！！！

--- 169/169.js
+++ 169/169.js
@@ -3,24 +3,19 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let string = nums.join(',');
-    string += ','
-    string = ',' + string;
+    // 利用js的对象
+    let o = {};
     for (let i = 0, len = nums.length; i < len; i++) {
-        // let reg = /(\d+)+/g;
-        // console.log(string.match(new RegExp(nums[i], 'g')).length);
-        let t = ',' + nums[i] + ',';
-        // console.log(string, t, i);
-        let cnt = 0;
-        let reg = new RegExp(t, 'g');
-        while (reg.exec(string)) {
-            reg.lastIndex--;
-            cnt++;
-            if (cnt > (~~(len / 2))) {
-                return + nums[i];
-            }
+        if (!o[nums[i]]) {
+            o[nums[i]] = 1;
+        }
+        else {
+            o[nums[i]]++;
+        }
+        if (o[nums[i]] > (~~(len / 2))) {
+            return nums[i];
         }
     }
 };
 
-console.log(majorityElement([1]));
+console.log(majorityElement([6]));
--- github.sh
+++ github.sh
@@ -1,4 +1,4 @@
 # commit shell，modify commit date before exec
 git add .
-git commit -m 'LeetCode每日一题,js版本：2017-08-19 今天没做，明天补上'
+git commit -m 'LeetCode每日一题,js版本：2017-09-04 罪孽呀，接了新项目，比较难，在适应期，然后每天很烦，竟然已经有半个月没有做题了！！！'
 git push
