1719353db055e2e16a24448222759842ebd3ab89
bmxklYzj <948228952@qq.com>
2017-09-06 00:10:48 +0800

    LeetCode每日一题,js版本：2017-09-05

--- 172/172.js
+++ 172/172.js
@@ -3,7 +3,17 @@
  * @return {number}
  */
 var trailingZeroes = function(n) {
-    let stringN = '' + n;
+    let f = [];
+    function fact(n) {
+        // console.log(n, f[n]);
+        if (f[n]) return f[n];
+        if (n === 0 || n === 1) return 1;
+        return f[n] = n * fact(n - 1);
+        // console.log(n * fact(n - 1), f[n]);
+    }
+
+    fact(n);
+    let stringN = '' + f[n];
     let cnt = 0;
     for (let i = stringN.length - 1; i > 0; i--) {
         if (stringN[i] === '0') {
@@ -13,6 +23,7 @@ var trailingZeroes = function(n) {
             break;
         }
     }
+
     return cnt;
 };
 
