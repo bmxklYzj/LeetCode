1ed7de43e0bf9871354944347a1bcd63070d9ff3
bmxklyzj <948228952@qq.com>
2020-03-28 23:57:37 +0800

    LeetCode每日一题, 2020-03-28

--- 2020-03-25_leetcode-892-三维形体的表面积/readme.md
+++ 2020-03-25_leetcode-892-三维形体的表面积/readme.md
@@ -1,10 +1,48 @@
 ## Description
 
+[892. 三维形体的表面积](https://leetcode-cn.com/problems/surface-area-of-3d-shapes/)
+
+grid表示立方体的高，求整体的表面积。
 
 
 ## Solution
 
+表面积 = 每行的高度 + 每列的高度 + grid[i][j]存在则 * 2
 
+先算每行、每列的高度，比如每行的高度，是头、尾高度 + Math.abs(grid[x][y] - grid[x][y - 1])
+
+```js
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var surfaceArea = function(grid) {
+  const n = grid[0].length;
+  let cnt = 0;
+  let arrX = new Array(n).fill(0);
+  let arrY = new Array(n).fill(0);
+  for (let x = 0; x < n; x++) {
+    for (let y = 0; y < n; y++) {
+      arrX[x] += Math.abs((y === 0 ) ? grid[x][y] : (grid[x][y] - (grid[x][y - 1])));
+      grid[x][y] && (cnt++);
+    }
+    arrX[x] += grid[x][n - 1];
+  }
+  
+  for (let y = 0; y < n; y++) {
+    for (let x = 0; x < n; x++) {
+      arrY[y] += Math.abs((x === 0) ? grid[x][y] : (grid[x][y] - (grid[x - 1][y])));
+    }
+    arrY[y] += grid[n - 1][y];
+  }
+  
+  return getSum(arrX) + getSum(arrY) + cnt * 2;
+};
+const getSum = arr => arr.reduce((acc, cur) => acc + cur, 0);
+```
 
 ### Time/Space complexity
 
+时间复杂度：O(N^2)
+
+空间复杂度：O(N)
--- 2020-03-25_leetcode-892-三维形体的表面积/solution.js
+++ 2020-03-25_leetcode-892-三维形体的表面积/solution.js
@@ -7,33 +7,30 @@
 var surfaceArea = function(grid) {
   const n = grid[0].length;
   let cnt = 0;
-  let arrX = new Array(n + 1).fill(0);
-  let arrY = new Array(n + 1).fill(0);
+  let arrX = new Array(n).fill(0);
+  let arrY = new Array(n).fill(0);
   for (let x = 0; x < n; x++) {
     for (let y = 0; y < n; y++) {
-      // console.log('Remove me in !dev: ', x,y, arrX[x]);
-  
-      arrX[x] = Math.abs((y === 0 || y === n - 1) ? grid[x][y] : (grid[x][y] - (grid[x][y - 1])));
-      // // // TODO: remove me
-      // console.log('Remove me in !dev: ', x,y, arrX[x], grid[x][y]);
-      
+      arrX[x] += Math.abs((y === 0 ) ? grid[x][y] : (grid[x][y] - (grid[x][y - 1])));
       grid[x][y] && (cnt++);
     }
-  }
-  for (let y = 0; y < n; y++) {
-    for (let x = 0; x < n; x++) {
-      arrY[y] = Math.abs((x === 0 || x === n - 1) ? grid[x][y] : (grid[x][y] - (grid[x - 1][y])));
-    }
+    arrX[x] += grid[x][n - 1];
   }
   
-  // console.log(arrX, arrY);
-  return getSum(arrX) * 2 + getSum(arrY) * 2 + cnt * 2;
+  for (let y = 0; y < n; y++) {
+    for (let x = 0; x < n; x++) {
+      arrY[y] += Math.abs((x === 0) ? grid[x][y] : (grid[x][y] - (grid[x - 1][y])));
+    }
+    arrY[y] += grid[n - 1][y];
+  }
+  
+  return getSum(arrX) + getSum(arrY) + cnt * 2;
 };
 const getSum = arr => arr.reduce((acc, cur) => acc + cur, 0);
 
+console.log(surfaceArea([[2]])); // 10
 console.log(surfaceArea([[1,0],[0,2]])); // 16
 console.log(surfaceArea([[1,2],[3,4]])); // 34
-console.log(surfaceArea([[2]])); // 10
 console.log(surfaceArea([[1,1,1],[1,0,1],[1,1,1]])); // 32
 console.log(surfaceArea([[2,2,2],[2,1,2],[2,2,2]])); // 46
 
